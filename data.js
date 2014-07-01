9942416e93a04f5d037568d32e1c4280c8de5aa2
Rachel Killackey <rkillac1@gmail.com>
2014-07-01 10:59:46 -0500

    Linking to JSON file

---
+++ data.js
@@ -0,0 +1,56 @@
+// Key plots job location. Y plots the total number of time for all jobs to complete.
+
+  var testdata = [
+    {
+      key: "buffalo.edu",
+      y: 57.80761574074074
+    },
+    {
+      key: "tier2",
+      y: 437.185625
+    },
+    {
+      key: "nd.edu",
+      y: 69.01653935185185
+    },
+    {
+      key: "unl.edu",
+      y: 1054.89125
+    },
+    {
+      key: "local",
+      y: 152.8890972222222
+    },
+    {
+      key: "iu.edu",
+      y: 183.15769675925924
+    },
+    {
+      key: "ucdavis.edu",
+      y: 40.76177083333333
+    },
+    {
+      key: "purdue.edu",
+      y: 192.94825231481482
+    },
+    {
+      key: "aglt2.org",
+      y: 161.65851851851852
+    },
+    {
+      key: "wisc.edu",
+      y: 147.29196759259258
+    },
+    {
+      key: "northwestern.edu",
+      y: 53.48828703703703
+    },
+    {
+      key: "smu.edu",
+      y: 149.71890046296298
+    },
+    {
+      key: "bnl.gov",
+      y: 15.867118055555556
+    },
+  ];
---
+++ data2.js
@@ -0,0 +1,32 @@
+ // Key plots a timestamp. Y plots the number of jobs running on condor at that time.
+  
+  var testdata = [
+    {
+      key: "1403634740",
+      y: 11521
+    },
+    {
+      key: "1403634752",
+      y: 11513
+    },
+    {
+      key: "1403634764",
+      y: 11511
+    },
+    {
+      key: "1403634776",
+      y: 11507
+    },
+    {
+      key: "1403634787",
+      y: 11430
+    },
+    {
+      key: "1403634799",
+      y: 11427
+    },
+    {
+      key: "1403634811",
+      y: 11421
+    }
+  ];
